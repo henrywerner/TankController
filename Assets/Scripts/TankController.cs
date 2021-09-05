9a1a88a6517988016644e0a34356801a5c611419
Henry <htwerner01@gmail.com>
2021-09-05 17:58:57 -0500

    Add mouse aim

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TankController : MonoBehaviour
 {
     [SerializeField] float _turnSpeed = 2f;
     [SerializeField] float _maxSpeed = 0.25f;
+    [SerializeField] private Image _xhair;
 
     Rigidbody _rb = null;
-    
-    
+    [SerializeField] GameObject _gun;
+
+
     public float MaxSpeed
     {
         get => _maxSpeed;
@@ -19,12 +22,16 @@ public class TankController : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+
+        if (_xhair == null)
+            print("crosshair not found");
     }
 
     private void FixedUpdate()
     {
         MoveTank();
         TurnTank();
+        AimLoop();
     }
 
     public void MoveTank()
@@ -46,5 +53,23 @@ public class TankController : MonoBehaviour
         Quaternion turnOffset = Quaternion.Euler(0, turnAmountThisFrame, 0);
         // apply quaternion to the rigidbody
         _rb.MoveRotation(_rb.rotation * turnOffset);
+    }
+
+    void AimLoop()
+    {
+        //TODO: move crosshair motion to higher in gameloop
+        _xhair.gameObject.transform.position = Input.mousePosition; // move the crosshair the current mouse position
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var plane = new Plane(Vector3.up, Vector3.zero);
+        
+        // Use raycast to convert mouse position into world position
+        if (plane.Raycast(ray, out var distance))
+        {
+            Vector3 reticle = ray.GetPoint(distance);
+            Vector3 direction = reticle - transform.position;
+            float rotation = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            _gun.transform.rotation = Quaternion.Euler(0, rotation, 0); // rotate gun towards mouse cursor
+        }
     }
 }
